d9e41b729d2c00d9e2ce8f117a5117506297f536
Ketan Rathod <ketanrtd1@gmail.com>
2024-01-24 17:13:51 +0530

    added arrays content

---
+++ Golang Programs/explore-golang/arrays.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hellow Arrays")
+
+	fmt.Println("why it is running faster idk may be its ubuntu thats why ha ha")
+
+	arr := [5]int{1, 2, 3, 4, 5}
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+
+	var arr2 [5]int
+	for i, v := range arr2 {
+		fmt.Println(i, v)
+	}
+}
