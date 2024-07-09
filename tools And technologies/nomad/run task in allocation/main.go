c544f7f8fd22195ef4f8ca9a54b938b800cedeb8
Ketan Rathod <ketanrtd1@gmail.com>
2024-07-09 14:23:10 +0530

    working example of running single command inside allocation completed

--- tools And technologies/nomad/run task in allocation/main.go
+++ tools And technologies/nomad/run task in allocation/main.go
@@ -2,90 +2,92 @@ package main
 
 import (
 	"context"
-	"io"
+	"fmt"
 	"log"
-	"os"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/hashicorp/nomad/api"
 )
 
-// CustomIO struct that implements io.Reader and io.Writer
-type CustomIO struct {
-	data []byte
-	mu   sync.Mutex
+// defining custom input/output struct
+
+type IO struct {
+	readBufferLock sync.Mutex
+	readBuffer     []byte // for the logic purpose
+	outputBuffer   []byte // for summarizing final output from the command
+	cancelFunc     context.CancelFunc
 }
 
-// NewCustomIo creates a new instance of CustomIO
-func NewCustomIo() *CustomIO {
-	return &CustomIO{
-		data: make([]byte, 0, 2048),
+func NewIO(cancelFunc context.CancelFunc) *IO {
+	return &IO{
+		readBuffer: make([]byte, 0, 2048),
+		cancelFunc: cancelFunc,
 	}
 }
 
-// Read implements the io.Reader interface
-func (c *CustomIO) Read(p []byte) (n int, err error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+// read method will be used by the nomad to read the command we sent to it.
+func (i *IO) Read(p []byte) (int, error) {
+	// time.Sleep(2 * time.Second)
+	i.readBufferLock.Lock()
+	defer i.readBufferLock.Unlock()
 
-	log.Println("Started reading")
+	n := copy(p, i.readBuffer)
+	i.readBuffer = i.readBuffer[n:]
 
-	if len(c.data) == 0 {
-		return 0, io.EOF
-	}
-
-	n = copy(p, c.data)
-	c.data = c.data[n:] // consume the data read
-
-	log.Println("Read:", string(p[:n]))
 	return n, nil
 }
 
-// Write implements the io.Writer interface
-func (c *CustomIO) Write(p []byte) (n int, err error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+// write method will be used by nomad to write the output of the command we sent
+func (i *IO) Write(p []byte) (int, error) {
+	// write or send message to anyone and return the length of the data sent
+	// fmt.Printf("%v", string(p))
 
-	c.data = append(c.data, p...)
-	n = len(p)
+	// NOTE: i can not cancel the context here because the nomad server is doing multiple writes for the same thing.
 
-	log.Println("Write:", string(p))
-	return n, nil
+	// task completed hence
+	// fmt.Println("cancelling context")
+	// i.cancelFunc()
+
+	i.outputBuffer = append(i.outputBuffer, p...)
+
+	// Alternate Solution
+	// Check that if it has 2 hashtags 1 at start and 1 at end then done ha ha
+
+	return len(p), nil
 }
+
+// read a single command from the user
+func (i *IO) readCommand(command string) {
+	i.readBufferLock.Lock()
+	defer i.readBufferLock.Unlock()
+
+	i.readBuffer = append(i.readBuffer, fmt.Sprintf("%s\n", command)...)
+}
+
 func main() {
-
-	// helper := NewCustomIo()
-
-	// helper.Write([]byte("good"))
-
-	// var p []byte = make([]byte, 1024)
-	// n, err := helper.Read(p)
-
-	// if err != nil {
-	// 	log.Println("err reading ")
-	// }
-
-	// log.Println(n, string(p))
-
 	// Initialize a new Nomad API client
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
 		log.Fatalf("failed to create Nomad client: %v", err)
 	}
-	var jobId string = "e0aafbc8-d408-4362-981e-9a310d28a656-2912f838-196b-4c77-a717-7311343e530d-2-minecraft-f0a6e961-a71f-48c2-a095-09b1cea85761"
+
+	// We have same jobId and Namespace for our project
+	var jobId string = "4a1cb83b-1c37-482c-82f9-36bd045164f3-a721cb72-7401-4db4-bcd4-480e5e9da69b-2-minecraft-b0c42615-456f-46e5-9142-c3c421fdea86"
 	var namespace string = jobId
 	allocs, _, err := client.Jobs().Allocations(jobId, true, &api.QueryOptions{
 		Namespace: namespace,
 	})
 
 	if err != nil {
-		log.Println("enable to get allocations")
+		log.Println("unable to get allocations:", err)
+		return
 	}
 
 	if len(allocs) == 0 {
-		panic("no allocations")
+		panic("no allocations found")
 	}
 
 	// sort allocations according to the time they are created
@@ -93,45 +95,53 @@ func main() {
 		return allocs[i].CreateTime > allocs[j].CreateTime
 	})
 
-	log.Println("Allocations found are ", allocs)
-
-	// Now we have got our allocation // lets play with it to run a command
-
-	// Info is used to retrieve a single allocation if we know the allocation id
+	log.Println("Allocations found are", allocs)
 
 	allocation, _, err := client.Allocations().Info(allocs[0].ID, &api.QueryOptions{Namespace: namespace})
 
 	if err != nil {
-		log.Panic("error in getting allocation", err)
+		log.Panic("error in getting allocation:", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	// Initialize io.Reader, io.Writer, and io.Writer
-	var stdin io.Reader = os.Stdin
-	var stdout io.Writer = os.Stdout
-	var stderr io.Writer = os.Stdout
-	var command []string = make([]string, 0)
-	command = append(command, "/bin/sh")
+	// initialize custom io
+	io := NewIO(cancel)
+
+	stdin := io
+	stdout := io
+	stderr := io
+
+	io.readCommand("ls -ltr")
+
+	command := []string{"/bin/sh"}
 	log.Println("Command we are executing is", command)
-
-	// issue command
-	// go func() {
-	// 	time.Sleep(100 * time.Millisecond)
-	// 	stdout.Write([]byte("ls\n"))
-
-	// }()
 
 	var termSizeCh chan api.TerminalSize
 
 	exitCode, err := client.Allocations().Exec(ctx, allocation, jobId, true, command, stdin, stdout, stderr, termSizeCh, &api.QueryOptions{Namespace: namespace})
 
-	// log.Println("Data available inside helper", string(helper.data))
-
 	if err != nil {
-		log.Fatal(err)
+		log.Println("error", err)
 	}
 
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("Printing io output buffer \n", formattedOutput(string(io.outputBuffer)))
+	fmt.Println("")
+
 	log.Println("Process exited with exit code", exitCode)
+}
+
+// gets the string between two # hashtags.
+func formattedOutput(s string) string {
+	start := strings.Index(s, "#")
+	end := strings.LastIndex(s, "#")
+
+	if start == -1 || end == -1 || start == end {
+		return "" // No valid substring found
+	}
+
+	return s[start+1 : end]
 }
